332c2501ef1c8cd3d67fe3b444c636a9d34cfe99
Gabriela Hernandez <gabbyh1896@gmail.com>
2023-12-11 17:39:18 -0700

    first commit

---
+++ SQL/Secrets.Sql notes.py
@@ -0,0 +1,15 @@
+#DQL - 
+
+#DDL - defining what the tables look like. 
+#   this is how we tell sql how to define the data
+
+#DML - Data manipulation language 
+
+#       we will spend alot of time above
+
+#DCL and TCL not really used alot.
+
+#Sql is english - it's the base of the languae. The different dialects are similar
+#some things will be read slightly different in the Sql languages
+
+#### git ignore file, will use to copy and paste 
